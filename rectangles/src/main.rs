fa134adbe64aa646d1233e31b822b3f262afdd7e
pauldria <paul@myraff.com>
2020-03-24 09:55:22 -0700

    Some form of inheritance.

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,6 +8,13 @@ impl Rectangle {
     fn area(&self) -> u32 {
         return self.width * self.height;
     }
+
+    fn square(size: u32) -> Rectangle {
+        return Rectangle {
+            width:  size,
+            height: size
+        };
+    }
 }
 
 fn main() {
@@ -16,9 +23,17 @@ fn main() {
         height: 50
     };
 
+    let square = Rectangle::square(40);
+
     println!("The rectangle is {:?}", rect1);
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
+    );
+
+    println!("The square is {:?}", square);
+    println!(
+        "The area of the square is {} square pixels.",
+        square.area()
     );
 }
