654701f7bf38ec3141066c17c33e222dcccd09b4
pauldria <paul@myraff.com>
2020-03-24 09:43:00 -0700

    Refactoring with a struct. Also did an explicit naming of the reference.

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,12 +1,22 @@
+struct Rectangle {
+    width:  u32,
+    height: u32
+}
+
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle {
+        width:  30,
+        height: 50
+    };
+
+    let ptr_to_rect1 = &rect1;
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(rect1)
+        area(ptr_to_rect1)
     );
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    return dimensions.0 * dimensions.1;
+fn area(rectangle: &Rectangle) -> u32 {
+    return rectangle.width * rectangle.height;
 }
